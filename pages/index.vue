fe9dbe7479b691fe036da28eb457ca30be35b795
Sibrand <sibrand@vespertool.com>
2022-09-23 16:20:50 +0200

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex bg-black items-center justify-center h-screen w-full">
+    <h1 class="text-white text-3xl">coming soon</h1>
+  </div>
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
